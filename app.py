404c8e20853f30d09acbd5cbd3c1078b422d095d
Sivadol-Mix <89507353+Sivadol-Mix@users.noreply.github.com>
2021-11-22 11:26:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,6 @@ def event_handle(event):
             replyObj = TextSendMessage(text="เรียนคอมพิวเตอร์")
             line_bot_api.reply_message(rtoken, replyObj)
         else :
-            replyObj = TextSendMessage(text=msg)
-            line_bot_api.reply_message(rtoken, replyObj)
             headers = request.headers
             json_headers = ({k:v for k, v in headers.items()})
             json_headers.update({'Host':'bots.dialogflow.com'})
